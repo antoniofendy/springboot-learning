09939a321722f04ff65edea86dc08f857d315cae
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-10 15:14:02 +0700

    Update ScopeConfiguration.java

--- PZN/exercise/src/main/java/com/fendy/exercise/ScopeConfiguration.java
+++ PZN/exercise/src/main/java/com/fendy/exercise/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.fendy.exercise;
 
+import com.fendy.exercise.data.Bar;
 import com.fendy.exercise.data.Foo;
+import com.fendy.exercise.scope.DoubletoneScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -17,4 +20,17 @@ public class ScopeConfiguration {
         return new Foo();
     }
 
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletoneScope());
+        return configurer;
+    }
+
+    @Scope("doubleton")
+    @Bean
+    public Bar bar() {
+        log.info("Create new bar");
+        return new Bar();
+    }
 }
