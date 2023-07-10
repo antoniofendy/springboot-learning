7b9e5b2a5ad260a0c090c3bfd887a80a9253a15a
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-10 16:34:11 +0700

    Create FooConfiguration.java

---
+++ PZN/exercise/src/main/java/com/fendy/exercise/configuration/FooConfiguration.java
@@ -0,0 +1,15 @@
+package com.fendy.exercise.configuration;
+
+import com.fendy.exercise.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FooConfiguration {
+
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+}
