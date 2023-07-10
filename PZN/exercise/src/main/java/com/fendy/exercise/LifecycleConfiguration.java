2dfc238923da1e854346c1f16969219e3e70a379
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-10 16:08:35 +0700

    Update LifecycleConfiguration.java

--- PZN/exercise/src/main/java/com/fendy/exercise/LifecycleConfiguration.java
+++ PZN/exercise/src/main/java/com/fendy/exercise/LifecycleConfiguration.java
@@ -1,15 +1,26 @@
 package com.fendy.exercise;
 
 import com.fendy.exercise.data.Connection;
+import com.fendy.exercise.data.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LifecycleConfiguration {
 
+    // config bean lifecycle not using bean annotation
     @Bean
     public Connection connection() {
         return new Connection();
+    }
+
+    // we can use config bean lifecycle annotation to run method when it initialized and destroyed
+    // easier than non bean lifecycle  annotation
+    // @Bean(initMethod = "start", destroyMethod = "stop")
+    @Bean
+    public Server server() {
+
+        return new Server();
     }
 
 }
