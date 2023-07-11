e62f5ef449b27d0311cb29933c558c26d5858989
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-11 12:37:07 +0700

    Update ComponentConfiguration.java

--- PZN/exercise/src/main/java/com/fendy/exercise/ComponentConfiguration.java
+++ PZN/exercise/src/main/java/com/fendy/exercise/ComponentConfiguration.java
@@ -1,7 +1,9 @@
 package com.fendy.exercise;
 
+import com.fendy.exercise.data.MultiFoo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = {
@@ -9,5 +11,6 @@ import org.springframework.context.annotation.Configuration;
         "com.fendy.exercise.repository",
         "com.fendy.exercise.configuration"
 })
+@Import(MultiFoo.class)
 public class ComponentConfiguration {
 }
