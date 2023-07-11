6890a3e6c56f9dae4b2dbac3c940887697a655e4
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-11 14:54:30 +0700

    Create InheritanceConfiguration.java

---
+++ PZN/exercise/src/main/java/com/fendy/exercise/InheritanceConfiguration.java
@@ -0,0 +1,10 @@
+package com.fendy.exercise;
+
+import com.fendy.exercise.service.MerchantServiceImpl;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(MerchantServiceImpl.class)
+public class InheritanceConfiguration {
+}
