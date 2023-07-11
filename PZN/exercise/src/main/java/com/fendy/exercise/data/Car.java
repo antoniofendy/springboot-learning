793cd1a5928b48ffac64f1a1eb1f9d7e49bb3a69
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-11 15:39:04 +0700

    Create Car.java

---
+++ PZN/exercise/src/main/java/com/fendy/exercise/data/Car.java
@@ -0,0 +1,17 @@
+package com.fendy.exercise.data;
+
+import com.fendy.exercise.aware.IdAware;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements IdAware {
+
+    @Getter
+    private String id;
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+}
