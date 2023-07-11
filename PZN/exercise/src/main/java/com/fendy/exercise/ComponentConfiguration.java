5902512742b10842680ec507f88887b28a5f196b
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-11 11:27:21 +0700

    Update ComponentConfiguration.java

--- PZN/exercise/src/main/java/com/fendy/exercise/ComponentConfiguration.java
+++ PZN/exercise/src/main/java/com/fendy/exercise/ComponentConfiguration.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {
         "com.fendy.exercise.service",
-        "com.fendy.exercise.repository"
+        "com.fendy.exercise.repository",
+        "com.fendy.exercise.configuration"
 })
 public class ComponentConfiguration {
 }
