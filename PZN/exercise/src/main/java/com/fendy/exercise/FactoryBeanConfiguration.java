7a307d5c12a1c6750a6875e01e9ce99941e8280b
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-11 14:33:35 +0700

    Create FactoryBeanConfiguration.java

---
+++ PZN/exercise/src/main/java/com/fendy/exercise/FactoryBeanConfiguration.java
@@ -0,0 +1,12 @@
+package com.fendy.exercise;
+
+import com.fendy.exercise.factory.PaymentGatewayClientFactoryBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+        PaymentGatewayClientFactoryBean.class
+})
+public class FactoryBeanConfiguration {
+}
