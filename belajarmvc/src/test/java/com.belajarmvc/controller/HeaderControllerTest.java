afe266ff1a478cad5f18655f1e0f64e3c35086c0
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-14 15:35:16 +0700

    Create HeaderControllerTest.java

---
+++ belajarmvc/src/test/java/com.belajarmvc/controller/HeaderControllerTest.java
@@ -0,0 +1,35 @@
+package com.belajarmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HeaderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testRequestHeader() throws Exception {
+        mockMvc.perform(
+                get("/header/token")
+                    .header("X-TOKEN", "FENDY")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("OK"))
+        );
+    }
+
+}
