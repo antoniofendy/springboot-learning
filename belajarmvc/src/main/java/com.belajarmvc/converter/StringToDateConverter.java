726ba8e5dc25d667ea9dcf8f20223845ef16d611
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-14 14:39:42 +0700

    Create StringToDateConverter.java

---
+++ belajarmvc/src/main/java/com.belajarmvc/converter/StringToDateConverter.java
@@ -0,0 +1,28 @@
+package com.belajarmvc.converter;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class StringToDateConverter implements Converter<String, Date> {
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public Date convert(String s) {
+        try {
+            return dateFormat.parse(s);
+        } catch (ParseException e) {
+            log.warn("Error convert date from string {}", s, e);
+            return null;
+        }
+    }
+}
