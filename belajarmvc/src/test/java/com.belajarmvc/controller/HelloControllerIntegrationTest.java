ae85d5f13b15d4eb7d0f64560b35782cf1ac99c3
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-14 14:43:15 +0700

    Create HelloControllerIntegrationTest.java

---
+++ belajarmvc/src/test/java/com.belajarmvc/controller/HelloControllerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.belajarmvc.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class HelloControllerIntegrationTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void helloGuest() {
+        String response = restTemplate.getForEntity("http://localhost:" + port + "/hello", String.class)
+                .getBody();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Hello world Guest", response.trim());
+    }
+
+    @Test
+    void helloName() {
+        String response = restTemplate.getForEntity("http://localhost:" + port + "/hello?name=Fendy", String.class)
+                .getBody();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Hello world Fendy", response.trim());
+    }
+
+}
