8afbdf2a25b7f080f06ee729058c7531b6ce3819
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-14 14:43:40 +0700

    Create HelloControllerTest.java

---
+++ belajarmvc/src/test/java/com.belajarmvc/controller/HelloControllerTest.java
@@ -0,0 +1,51 @@
+package com.belajarmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloGuest() throws Exception {
+        mockMvc.perform(
+                get("/hello")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Guest"))
+        );
+    }
+
+    @Test
+    void helloName() throws Exception {
+        mockMvc.perform(
+                get("/hello").queryParam("name", "Fendy")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Fendy"))
+        );
+    }
+
+    @Test
+    void testHelloPost() throws Exception{
+        mockMvc.perform(
+                post("/hello")
+        ).andExpectAll(
+                status().isMethodNotAllowed()
+        );
+    }
+
+}
